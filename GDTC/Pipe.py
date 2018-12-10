9f54087e0cc3b14f7aa187bb05e0740462060733
Víctor Fernández Melic <melic93@gmail.com>
2018-12-10 11:39:30 +0100

    Pipe updated to use with clausule

--- GDTC/Pipe.py
+++ GDTC/Pipe.py
@@ -17,22 +17,23 @@ class Pipe():
         self.table = table
 
     def connectDB(self):
-         # Connect with db
-        try:
-            self.conn = psycopg2.connect(host=self.host, port=self.port, database=self.db, user=self.user, password=self.password)
-        except:
-            print('Error trying to connect to {} on {}'.format(self.db, self.user + ':' + self.password + '@' + self.host + ':' + self.port))
+
+        # Connect with db
+        with psycopg2.connect(host=self.host, port=self.port, database=self.db, user=self.user, password=self.password) as conn:
+            self.conn = conn
 
     def execQuery(self, sql_query):       
         # Execute query
-        cur = self.conn.cursor()
-        cur.execute(sql_query)
+        with self.conn.cursor() as cur:
+            cur.execute(sql_query)
         
-        if cur.description == None:
-            return None
-        else:
-            return cur.fetchall()        
+            if cur.description == None:
+                return None
+            else:
+                return cur.fetchall()        
 
     def closeDB(self):
         # Close connection with DB
-        self.conn.cursor().close()
+        with self.conn as conn:
+            conn.close()
+            
