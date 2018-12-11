22307a8556a4fe6e86950e2b7a5cb619e8c840ed
Víctor Fernández Melic <melic93@gmail.com>
2018-12-11 10:46:37 +0100

    Minor update

--- GDTC/Filters/transformation.py
+++ GDTC/Filters/transformation.py
@@ -13,28 +13,28 @@ import subprocess
 import psycopg2
 from subprocess import call
 
-def convertSHP2HDF(pipe, file_name):
+def convertSHP2HDF(pipe, file_name, *params):
     """ This function converts an SPH file from db specified on pipe to an HDF file and stores it into the db specified on pipe """
         
     print('convertSHP2HDF: Mock execution correct')
 
     return pipe
 
-def mergeHDFwithHDF(pipe, file_name1, file_name2):
+def mergeHDFwithHDF(pipe, file_name1, file_name2, *params):
     """ This functions merges two HDF files from db specified on pipe and stores it into db specified on pipe """
             
     print('mergeHDFwithHDF: Mock execution correct')
 
     return pipe
 
-def clipHDFwithSHP(pipe, hdf_file_name, shp_file_name):
+def clipHDFwithSHP(pipe, hdf_file_name, shp_file_name, *params):
     """ This function clips an HDF file with SHP boundaries, both from db specified on pipe and stores the result into that db """
             
     print('clipHDFwithSHP: Mock execution correct')
 
     return pipe
 
-def clipHDFwithCoordiantes(pipe, hdf_file_name, coord):
+def clipHDFwithCoordiantes(pipe, hdf_file_name, coord, *params):
     """ This function clips an HDF file from db specified on pipe with boundaries specified by coordinates and stores the result into the db specified on pipe """
             
     print('clipHDFwithCoordiantes: Mock execution correct')
