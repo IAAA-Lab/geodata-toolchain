c37769137a0b868b2f680ffc94b28ea705e0c6c8
Víctor Fernández Melic <melic93@gmail.com>
2019-01-14 13:42:32 +0100

    Clean up

--- luigi/workflow.py
+++ luigi/workflow.py
@@ -2,9 +2,12 @@ import luigi
 from load import insertHDF
 from DB import Db
 
-db = Db('127.0.0.1', '8432', 'postgres', 'geodatatoolchainps', 'postgres', 'geo-rasters2', '5')
+# Workflow example
 
-# Run workflow
+# Final db
+db = Db('127.0.0.1', '8432', 'postgres', 'geodatatoolchainps', 'postgres', 'geo-rasters2', '1')
+
+# Build and run workflow
 if __name__ == '__main__':
     luigi.build([insertHDF(file_name='MCD12Q1.A2006001.h17v04.006.2018054121935', db=db)], local_scheduler=True)
     luigi.run()
