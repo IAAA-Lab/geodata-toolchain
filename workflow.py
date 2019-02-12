e38d836d10aaab1a6cf3de2a4d5bd59c4847c337
Víctor Fernández Melic <melic93@gmail.com>
2019-02-12 13:29:26 +0100

    Restructured code

--- workflow.py
+++ workflow.py
@@ -1,36 +1,14 @@
-# Author: Víctor Fernández Melic
-# Project: IAAA GeoData Toolchain
-# Module: Workflow
-# Description: This module is where the user can define a complete workflow composed by one or many filters.
+import luigi
 
-from GDTC.Pipe import Pipe
-import GDTC.Filters.input as i
-import GDTC.Filters.output as o
-from GDTC.Stage import Stage
-from GDTC.decorators import run
+from gdtc_extension.gdtc_core.insert_hdf import insertHDF
+from gdtc_extension.gdtc_base.db import Db
 
-# Define initial Pipe
-pipe = Pipe('127.0.0.1', '8432', 'postgres', 'geodatatoolchainps', 'postgres', 'geo-rasters')
+# Workflow example
 
-# First approach of functional style workflow
-# It's not possible to instpect this because lambda functions has no name or docstring
+# Final db
+db = Db('127.0.0.1', '8432', 'postgres', 'geodatatoolchainps', 'postgres', 'geo-rasters', '5')
 
-s1 = Stage(pipe)
-s1.add(lambda: o.loadHDF(pipe, 'MCD12Q1.A2006001.h17v04.006.2018054121935.hdf', 1,''))\
-  .add(lambda: o.loadSHP(pipe, 'Comunidades_Autonomas_ETRS89_30N'))\
-  .add(lambda: o.loadSHP(pipe, 'Comunidades_Autonomas_ETRS89_30N'))\
-# .run()
-
-# Second approach of functional style workflow
-# This approach is better because it lets the inspection of the function and it's
-# easier to hide the use of pipes. It's also easier for the user beacuse there's no
-# need to use lambda functions
-
-s2 = Stage(pipe)
-s2.configure('concurrent')\
-  .add(o.loadHDF, 'MCD12Q1.A2006001.h17v04.006.2018054121935.hdf', 1)\
-  .add(o.loadHDF, 'Comunidades_Autonomas_ETRS89_30N')\
-  .add(o.loadHDF, 'Comunidades_Autonomas_ETRS89_30N')\
-  .inspect()\
-  .run()\
-  .inspect()
+# Build and run workflow
+if __name__ == '__main__':
+    luigi.build([insertHDF(file_name='MCD12Q1.A2006001.h17v04.006.2018054121935', db=db)], local_scheduler=False, log_level="DEBUG")
+    luigi.run()
